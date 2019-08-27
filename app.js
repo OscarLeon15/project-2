1031f0969393d7b27c9bfffb0fe46b869b5a6a08
oscar leon <oscar.leon.delmar@gmail.com>
2019-08-26 19:48:37 -0500

    add4

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
@@ -14,118 +13,81 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require('./models/User')
-
-const session      = require("express-session");
-const MongoStore= require("connect-mongo")(session);
-const passport     = require("passport");
-const LocalStrategy= require("passport-local").Strategy;
-
-// const User         = require('./models/User');
-const bcrypt       = require('bcryptjs');
-const flash        = require("connect-flash");
-
-
 mongoose
-  .connect('mongodb://localhost/project-2', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
+ .connect('mongodb://localhost/project-2', {useNewUrlParser: true})
+ .then(x => {
+   console.log(`Connected to Mongo! Database name:  ${x.connections[0].name}`)
+ })
+ .catch(err => {
+   console.error('Error connecting to mongo', err)
+ });
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 const app = express();
-
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true
+ secret: "our-passport-local-strategy-app",
+ resave: true,
+ saveUninitialized: true
 }));
-
 passport.serializeUser((user, cb) => {
-  cb(null, user._id);
+ cb(null, user._id);
 });
-
 passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
+ User.findById(id, (err, user) => {
+   if (err) { return cb(err); }
+   cb(null, user);
+ });
 });
-
 app.use(flash());
 passport.use(new LocalStrategy((username, password, next) => {
-  // passReqToCallback: true;
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
-
-    return next(null, user);
-  });
+ // passReqToCallback: true;
+ User.findOne({ username }, (err, user) => {
+   if (err) {
+     return next(err);
+   }
+   if (!user) {
+     return next(null, false, { message: "Incorrect username" });
+   }
+   if (!bcrypt.compareSync(password, user.password)) {
+     return next(null, false, { message: "Incorrect password" });
+   }
+   return next(null, user);
+ });
 }));
 // app.js
 app.use(passport.initialize());
 app.use(passport.session());
-
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+ src:  path.join(__dirname, 'public'),
+ dest: path.join(__dirname, 'public'),
+ sourceMap: true
 }));
-      
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-
 // default value for title local
 app.locals.title = 'When will this app have a decent app homepage?';
-
 const index = require('./routes/index');
 app.use('/', index);
-
 // app.js
 // Mongoose configuration
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/project-2')
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
+ .connect('mongodb://localhost/project-2')
+ .then(() => {
+   console.log('Connected to Mongo!')
+ }).catch(err => {
+   console.error('Error connecting to mongo', err)
+ });
 // ...other code
-
 // Routes
 const router = require("./routes/users");
 app.use('/', router);
-
-const teamsRoutes = require('./routes/teams');
-app.use('/teams',teamsRoutes);
-
 module.exports = app;
-
-
-
