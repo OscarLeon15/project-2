348ff9a55dba9c5cf56d4ceab78cf4d7db29eb91
Alejandro Salas <asalas.wd@outlook.com>
2019-08-28 20:54:17 -0400

    change token

--- public/javascripts/league-teams.js
+++ public/javascripts/league-teams.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $.ajax({
-  headers: { 'X-Auth-Token': 'process.env.TOKEN' },
+  headers: { 'X-Auth-Token': '0b56687d125b4f95bb31c75d9ad11a82' },
   url: 'http://api.football-data.org/v2/competitions/2014/teams',
   dataType: 'json',
   type: 'GET',
