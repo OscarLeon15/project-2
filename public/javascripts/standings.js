0b9c002fec7c58c7bc3587edccf048a08aa06a5b
Giba <you@example.com>
2019-08-28 20:52:12 -0400

    Added premier and serie A standings

--- public/javascripts/standings.js
+++ public/javascripts/standings.js
@@ -1,3 +1,5 @@
+//LaLiga call
+
 $(document).ready(function(){
   $.ajax({
     headers: { 'X-Auth-Token': '0b56687d125b4f95bb31c75d9ad11a82' },
@@ -8,7 +10,61 @@ $(document).ready(function(){
     console.log(response['standings'])
    
     for(let i = 0; i < response['standings'][0]['table'].length; i++){
-$("tbody").append(`<tr class="table-danger"> <td>${response['standings'][0]['table'][i].position}</td> <td><img style='height:42px; width:42px' 
+$("#Liga").append(`<tr class="table-primary"> <td>${response['standings'][0]['table'][i].position}</td> <td><img style='height:42px; width:42px' 
+src='${response['standings'][0]['table'][i].team.crestUrl}'> </td>  
+      
+                                                                  
+      //                                                          <td>${response['standings'][0]['table'][i].team.name}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].playedGames}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].won}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].draw}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].lost}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].points}</td></tr>`)
+    }
+ 
+  })
+});
+
+
+//Serie A call
+
+$(document).ready(function(){
+  $.ajax({
+    headers: { 'X-Auth-Token': '0b56687d125b4f95bb31c75d9ad11a82' },
+    url: 'https://api.football-data.org/v2/competitions/2019/standings',
+    dataType: 'json',
+    type: 'GET',
+  }).done(function(response) {
+    console.log(response['standings'])
+   
+    for(let i = 0; i < response['standings'][0]['table'].length; i++){
+$("#Serie").append(`<tr class="table-warning"> <td>${response['standings'][0]['table'][i].position}</td> <td><img style='height:42px; width:42px' 
+src='${response['standings'][0]['table'][i].team.crestUrl}'> </td>  
+      
+                                                                  
+      //                                                          <td>${response['standings'][0]['table'][i].team.name}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].playedGames}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].won}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].draw}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].lost}</td>
+      //                                                          <td>${response['standings'][0]['table'][i].points}</td></tr>`)
+    }
+ 
+  })
+});
+
+//Premier call
+$(document).ready(function(){
+  $.ajax({
+    headers: { 'X-Auth-Token': '0b56687d125b4f95bb31c75d9ad11a82' },
+    url: 'https://api.football-data.org/v2/competitions/2021/standings',
+    dataType: 'json',
+    type: 'GET',
+  }).done(function(response) {
+    console.log(response['standings'])
+   
+    for(let i = 0; i < response['standings'][0]['table'].length; i++){
+$("#Premier").append(`<tr class="table-danger"> <td>${response['standings'][0]['table'][i].position}</td> <td><img style='height:42px; width:42px' 
 src='${response['standings'][0]['table'][i].team.crestUrl}'> </td>  
       
                                                                   
--- routes/standings-route.js
+++ routes/standings-route.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 
-/* GET home page */
-router.get('/standings', (req, res, next) => {
-  res.render('static-views/standings');
+
+router.get('/standingsLaLiga', (req, res, next) => {
+  res.render('static-views/standingsLiga');
+});
+
+router.get('/standingsSerieA', (req, res, next) => {
+  res.render('static-views/standingsSerie');
+});
+
+router.get('/standingsPremier', (req, res, next) => {
+  res.render('static-views/standingsPremier');
 });
 
 module.exports = router;
