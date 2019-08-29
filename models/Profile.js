4130f0abdd10d9c3443e0ae688677f2a495074e0
Alejandro Salas <asalas.wd@outlook.com>
2019-08-28 20:17:39 -0400

    add Profile.js model

---
+++ models/Profile.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+    name: String,
+    favoriteTeams: [],
+    picture: String    
+})
+
+
+
+const profileModel = mongoose.model('Profile', profileSchema)
+//mongoose expects the name of the model to be singular and have a capital first letter
+//name of the collection in DB will be called profiles with lower case P because mongoose will do it by magic
+
+module.exports = profileModel;
