50dc84ac87187d8dc1f3b0117fb1784fef5bcc3f
Alejandro Salas <asalas.wd@outlook.com>
2019-09-01 20:26:11 -0400

    update Profile model

--- models/Profile.js
+++ models/Profile.js
@@ -7,8 +7,8 @@ const profileSchema = new Schema({
     picture: String    
 })
 
-const profileModel = mongoose.model('Profile', profileSchema)
+const Profile = mongoose.model('Profile', profileSchema)
 //mongoose expects the name of the model to be singular and have a capital first letter
 //name of the collection in DB will be called profiles with lower case P because mongoose will do it by magic
 
-module.exports = profileModel;
+module.exports = Profile;
