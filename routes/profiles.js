11ea4c6eebf757a35702d54723cb965e04f66943
Alejandro Salas <asalas.wd@outlook.com>
2019-09-02 12:40:51 -0400

    add comments to profile route and create-profile hbs

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,14 +5,13 @@ const Profile = require('../models/Profile');
 /* GET profile page */
 // TODO: add call to API load teams
 router.get('/private/create-profile', (req, res, next) => {  
+  // load username information from req.user to separate profiles created by each user
   res.render('user/create-profile', { username: req.user.username });
 });
 
 // POST request to add new profile
 router.post('/create-profile', (req, res, next) => {
-  const { username, name, favoriteTeams, picture} = req.body;
-  console.log(req.user.username);
-  console.log(req.body);
+  const { username, name, favoriteTeams, picture} = req.body;  
 
   const newProfile = new Profile({ username, name, favoriteTeams, picture});
   newProfile.save()
