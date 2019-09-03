e2c5cc2a97f3932c21e8a78d07483c4e5411fd3b
Alejandro Salas <asalas.wd@outlook.com>
2019-09-02 20:23:24 -0400

    changed favoriteTeams to favoriteTeam and from array to string

--- models/Profile.js
+++ models/Profile.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const profileSchema = new Schema({
     username: String,
     name: String,
-    favoriteTeams: [],
+    favoriteTeam: String,
     picture: { type: String, default: 'https://res.cloudinary.com/alejotsv/image/upload/v1567385712/no-avatar.png' }
 }, {
     timestamps: true
