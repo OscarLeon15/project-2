92bf520362bca1125d54f3c374f443cfeaff866d
Alejandro Salas <asalas.wd@outlook.com>
2019-09-08 23:08:56 -0400

    update profile creation logic

---
+++ models/League.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ligaSchema = new Schema({
+  area: {
+    id: Number,
+    name: String
+},
+    id: Number,  
+    name: String,
+    shortName: String,
+    crestUrl: String,
+    website: String,
+    founded: Number,
+    venue: String
+})
+
+
+
+const League = mongoose.model('League', ligaSchema)
+//mongoose expects the name of the model to be singular and havea capital first letter
+//name of the collection in DB will be called celebrities with lower case C because mongoose will do it by magic
+
+
+module.exports = League;
