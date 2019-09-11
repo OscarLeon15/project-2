10bd04b249f8f0701c1f0dde83c80cf3fecad7ca
Alejandro Salas <asalas.wd@outlook.com>
2019-09-10 23:38:07 -0400

    troubleshoot standings

--- routes/standings-route.js
+++ routes/standings-route.js
@@ -5,8 +5,14 @@ const router  = express.Router();
 
 router.get('/standings/:league', (req, res, next) => {
   const leagueName = req.params.league;
-  console.log(leagueName);
-    res.render('static-views/standings', { name });
+  if (leagueName === 'Spain'){
+    res.render('static-views/standings');
+  } else if (leagueName === 'Italy'){
+    res.render('static-views/standingsSerie');
+  } else if (leagueName === 'England'){
+    res.render('static-views/standingsPremier');
+  }
+    
 });
 
 
