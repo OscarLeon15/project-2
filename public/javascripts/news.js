92149cd5ed59a08eb5dd663556dba8a6f77213a5
Alejandro Salas <asalas.wd@outlook.com>
2019-09-11 22:49:25 -0400

    update api request for news

--- public/javascripts/news.js
+++ public/javascripts/news.js
@@ -8,13 +8,10 @@ $(document).ready(function(){
           method: "GET",
           type: "json",
           url: 'https://newsapi.org/v2/everything?' +
-          'from=2019-09-10&' +
-          'q=' + team + '& soccer' + 
-          'sources=bbc-sport,fox-sports, marca, mtv-news-uk, el-mundo, talksport, reuters, the-telegraph, associated-press, football-italia, lequipe, ansa, google-news, bleacher-report' +
-          'language=en' +
-          'category=sports' +
-          'sortBy=relevancy&' +
-          'apiKey=308d8f4e99ae4cf8b0fa8709b91948c4'
+          'q=' + team +  
+          '&language=en' +          
+          '&sortBy=relevancy' +
+          '&apiKey=308d8f4e99ae4cf8b0fa8709b91948c4'
                     
         }).done(function(response) {  
          
