dd9925518f7d7a80a619db6c687644064aa47473
oscar leon <oscar.leon.delmar@gmail.com>
2020-01-03 09:31:32 -0600

    volnerable dependencies update hbs and mongoose

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+
 const bcrypt      = require('bcryptjs');
 const flash = require("connect-flash");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+
 const User = require('./models/User');
 const Profile = require('./models/Profile');
 
@@ -25,6 +27,7 @@ mongoose
  .catch(err => {
    console.error('Error connecting to mongo', err)
  });
+ 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 const app = express();
